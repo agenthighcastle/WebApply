bfe1aca2cfb38d26712672279ff216e5b8068480
High Castle <jona@keemail.me>
2019-04-26 18:43:16 +0200

    Removal of .php extension

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ require('assets/languages/lang_'.getSetting("lang").'.php');
 require $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "core/_header.php";
 ?>
   <body>
-  <a href="login.php" class="loginicon"><i class="fas fa-sign-in-alt"></i></a>
+  <a href="login" class="loginicon"><i class="fas fa-sign-in-alt"></i></a>
   <section id="intro">
 	<div class="content">
 	  <div class="container">
@@ -27,13 +27,13 @@ require $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "core/_header.php";
 	<div class="container">
 		<div class="row">
 			<div class="col-md-4">
-			<p><a href="apply.php?type=supporter" class="btn btn-green">Supporter</a></p>
+			<p><a href="apply?type=supporter" class="btn btn-green">Supporter</a></p>
 			</div>
 			<div class="col-md-4">
-			<p><a href="apply.php?type=developer" class="btn btn-blue">Developer</a></p>
+			<p><a href="apply?type=developer" class="btn btn-blue">Developer</a></p>
 			</div>
 			<div class="col-md-4">
-			<p><a href="apply.php?type=builder" class="btn btn-orange">Builder</a></p>
+			<p><a href="apply?type=builder" class="btn btn-orange">Builder</a></p>
 			</div>
 		</div>
 	</div>
